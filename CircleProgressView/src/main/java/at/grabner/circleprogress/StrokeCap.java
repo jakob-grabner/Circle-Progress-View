82f70f418c9c33e3cf0c24946f5d945226368c18
Ted Schaefer <6bangs@gmail.com>
2016-07-06 14:33:49 -0700

    Add cpv_barStrokeCap attribute.

---
+++ CircleProgressView/src/main/java/at/grabner/circleprogress/StrokeCap.java
@@ -0,0 +1,26 @@
+package at.grabner.circleprogress;
+
+import android.graphics.Paint;
+
+public enum StrokeCap {
+   /**
+    * The stroke ends with the path, and does not project beyond it.
+    */
+   BUTT    (Paint.Cap.BUTT),
+   /**
+    * The stroke projects out as a semicircle, with the center at the
+    * end of the path.
+    */
+   ROUND   (Paint.Cap.ROUND),
+   /**
+    * The stroke projects out as a square, with the center at the end
+    * of the path.
+    */
+   SQUARE  (Paint.Cap.SQUARE);
+
+   final Paint.Cap paintCap;
+
+   StrokeCap(Paint.Cap paintCap) {
+      this.paintCap = paintCap;
+   }
+}
